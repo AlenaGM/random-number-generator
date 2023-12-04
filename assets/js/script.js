551133e75179fa1c1cb1b2360a8c701e5e969003
AlenaGM <alena.guillaume4@gmail.com>
2023-12-04 13:39:46 +0100

    upd img anf readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,31 @@
+/* eslint-disable no-undef */
 const array = [];
 
-document.querySelector("#generate").addEventListener("click", function(event) {
-    event.preventDefault();
-    generateRND();
+document.querySelector("#generate").addEventListener("click", function (event) {
+  event.preventDefault();
+  generateRND();
 });
 
-function generateRND(){
+function generateRND() {
+  const min = Math.ceil(-10);
+  const max = Math.floor(10);
 
-    const min = Math.ceil(-10);
-    const max = Math.floor(10);
+  for (i = 0; i < 10; i++)
+    array[i] = Math.floor(Math.random() * (max - min)) + min;
 
-    for (i = 0; i < 10; i++)
-    array [i] = Math.floor(Math.random() * (max - min)) + min;
+  document.querySelector("#rnd").value = array;
+  document.querySelector("#min").value = Math.min.apply(null, array);
+  document.querySelector("#max").value = Math.max.apply(null, array);
 
-    document.querySelector('#rnd').value = array;
-    document.querySelector('#min').value = Math.min.apply(null,array);
-    document.querySelector('#max').value = Math.max.apply(null,array);
+  let sum = 0;
+  let mpy = 1;
 
-    let sum = 0;
-    let mpy = 1;
+  for (i = 0; i < 10; i++) {
+    sum += array[i];
+    mpy *= array[i];
+  }
 
-    for (i=0; i < 10; i++) {
-        sum += array[i];
-        mpy *= array[i];
-    }
-
-    document.querySelector('#avg').value = sum / array.length;
-    document.querySelector('#sum').value = sum;
-    document.querySelector('#mpy').value = mpy;
+  document.querySelector("#avg").value = sum / array.length;
+  document.querySelector("#sum").value = sum;
+  document.querySelector("#mpy").value = mpy;
 }
-
